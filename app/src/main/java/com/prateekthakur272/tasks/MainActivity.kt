658dd8b191ae836b7843e1561c35e996763c78d5
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2022-10-24 00:49:19 +0530

    Logo added

--- app/src/main/java/com/prateekthakur272/tasks/MainActivity.kt
+++ app/src/main/java/com/prateekthakur272/tasks/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.prateekthakur272.tasks
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val introImages:ImageView = findViewById(R.id.intro_images)
+        val imageArray:Array<Int> = arrayOf(R.drawable.cat,R.drawable.crocodile,R.drawable.elephant,R.drawable.monkey,R.drawable.sea_horse)
+        var image = 0
+        introImages.setOnClickListener {
+            introImages.setImageResource(imageArray[image%imageArray.size])
+            image++
+        }
     }
 }
