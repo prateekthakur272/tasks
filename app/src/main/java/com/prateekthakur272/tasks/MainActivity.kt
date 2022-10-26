7eb05b1cca48ce4e02081eea0af4257937c9dcca
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2022-10-26 12:22:30 +0530

    Splash screen created

--- app/src/main/java/com/prateekthakur272/tasks/MainActivity.kt
+++ app/src/main/java/com/prateekthakur272/tasks/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.prateekthakur272.tasks
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,12 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         val introImages:ImageView = findViewById(R.id.intro_images)
         val imageArray:Array<Int> = arrayOf(R.drawable.cat,R.drawable.crocodile,R.drawable.elephant,R.drawable.monkey,R.drawable.sea_horse)
         var image = 0
-        introImages.setOnClickListener {
-            introImages.setImageResource(imageArray[image%imageArray.size])
-            image++
-        }
+        val handler = Handler()
+        Thread {
+            while (true) {
+                introImages.setImageResource(imageArray[image++ % imageArray.size])
+                Thread.sleep(500)
+            }
+        }.start()
+        handler.postDelayed({finish()},4000)
     }
 }
