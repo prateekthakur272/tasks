e9a4ef94c52091033382e1de91410643cc73df02
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2022-10-28 13:33:20 +0530

    Add Task Activity created

--- app/src/main/java/com/prateekthakur272/tasks/AddTask.kt
+++ app/src/main/java/com/prateekthakur272/tasks/AddTask.kt
@@ -6,43 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
-import android.widget.DatePicker
-import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TimePicker
-import java.util.Random
+import java.util.*
 
-class AddTask : AppCompatActivity(),DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener{
+class AddTask : AppCompatActivity(){
+    private val calendar:Calendar = Calendar.getInstance()
+    private lateinit var datePickerDialog:DatePickerDialog
+    private lateinit var timePickerDialog: TimePickerDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
 
-        val imageArray:Array<Int> = arrayOf(R.drawable.cat,R.drawable.crocodile,R.drawable.elephant,R.drawable.monkey,R.drawable.sea_horse)
-
         val addDateTime:CheckBox =findViewById(R.id.add_date_time)
         val dateTimeLayout:LinearLayout = findViewById(R.id.date_time_layout)
-        val image:ImageView = findViewById(R.id.image)
-        image.setImageResource((imageArray).random())
+
+        datePickerDialog = DatePickerDialog(this)
+        timePickerDialog = TimePickerDialog(this,
+            { _, h, m ->  },calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),false)
 
         addDateTime.setOnCheckedChangeListener { _, b ->
-            if (b)
+            if (b){
+                datePickerDialog.show()
                 dateTimeLayout.visibility = View.VISIBLE
+            }
             else
                 dateTimeLayout.visibility = View.GONE
         }
-//        val datePickerDialog:DatePickerDialog = DatePickerDialog(this)
-//        datePickerDialog.show()
-    }
-
-    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
-//        val calendar = Calendar.getInstance()
-//        val timePickerDialog = TimePickerDialog(this,
-//            { timePicker, h, m ->  },calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),false)
-//        timePickerDialog.show()
-
-    }
-
-    override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {
-
+        datePickerDialog.setOnDateSetListener { datePicker, i, i2, i3 ->
+            timePickerDialog.show()
+        }
     }
 }
