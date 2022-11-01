62a7b363274e115e534c058dd46eaefc72dd8f5f
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2022-11-01 12:15:31 +0530

    Colors Darkened, popups created and added more images to splash animation

---
+++ app/src/main/java/com/prateekthakur272/tasks/DateTime.kt
@@ -0,0 +1,22 @@
+package com.prateekthakur272.tasks
+enum class MonthName{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+class DateTime {
+    var day = 1
+    var month = MonthName.January
+    var year = 2022
+    var hour = 0
+    var minute = 0
+}
--- app/src/main/java/com/prateekthakur272/tasks/Task.kt
+++ app/src/main/java/com/prateekthakur272/tasks/Task.kt
@@ -1,7 +1,9 @@
 package com.prateekthakur272.tasks
 
+import java.time.format.DateTimeFormatter
 
 class Task {
     lateinit var title:String
     var description: String? = null
+
 }
